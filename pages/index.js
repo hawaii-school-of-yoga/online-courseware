206a6b6db28c8b04bccf7c8f76eacda301db31f7
awitherow <au.witherow@gmail.com>
2018-12-25 11:19:58 -1000

    get basic player up and running.

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,7 @@
-export default () => <div>Welcome to next.js!</div>;
+import Video from '../components/Video';
+
+export default () => (
+	<div>
+		<Video url="www.youtube.com/watch?v=b5H3b_Hh0Lw" />
+	</div>
+);
