c14ff61820c5bdf05a8487186ec9f9dbd37f6a56
awitherow <au.witherow@gmail.com>
2018-12-29 09:21:59 -1000

    document prettier

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,10 @@ export default class MyDocument extends Document {
 		return (
 			<html>
 				<Head>
-					<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1"
+					/>
 					<link
 						rel="stylesheet"
 						href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
