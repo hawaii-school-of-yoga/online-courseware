9de02fd3be5ed72b54c8efeecbf6139e04bb0cb2
awitherow <au.witherow@gmail.com>
2019-01-15 19:49:04 -1000

    add basic view for editing email, username

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -2,14 +2,50 @@ import React, { Component } from 'react';
 
 import Main from '../../components/templates/Main';
 
+import Button from '../../components/atoms/Button';
+
+import './profile.scss';
+
 export default class Modules extends Component {
 	render() {
 		const { data, state, updateState } = this.props;
 
+		console.log(data, state);
+
 		return (
 			<div id="page--profile">
 				<Main updateState={updateState} user={data.user} state={state}>
-					Hello, world!
+					<div className="title-bar">
+						<h1>Your Hawaii School of Yoga Profile</h1>
+						<hr className="primary" />
+					</div>
+					<form className="edit-profile">
+						<h2>Edit Your Profile</h2>
+						<label htmlFor="inputEmail" className="sr-only">
+							Email address
+						</label>
+						<input
+							type="email"
+							id="inputEmail"
+							className="form-control"
+							placeholder={data.user.email}
+						/>
+						<label htmlFor="inputUsername" className="sr-only">
+							Username
+						</label>
+						<input
+							type="text"
+							id="inputUsername"
+							className="form-control"
+							placeholder={data.user.name}
+						/>
+						<Button color="success" onClick={() => {}}>
+							Save Changes
+						</Button>
+					</form>
+					<h2 className="my-courses">
+						<h2>My Courses</h2>
+					</h2>
 				</Main>
 			</div>
 		);
